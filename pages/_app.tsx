d437c5df338e7f6e0e8e290f4d98bf57e03390c6
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2023-07-09 18:05:42 +0800

    setup folder structure, caching, layouting, and routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,32 @@
-import './globals.css'
-import { Inter, Montserrat } from 'next/font/google'
+import "./globals.css";
+import Layout from "./layout";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../config/createEmotionCache";
+import Head from "next/head";
 
-const inter = Montserrat({ subsets: ['latin'] })
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 export const metadata = {
-  title: 'ENITIO 2023',
-  description: 'Honestly now is Loren Ipsum',
-}
+  title: "ENITIO 2023",
+  description: "Honestly now is Loren Ipsum",
+};
 
 export default function RootLayout({
-  Component, pageProps
+  Component,
+  pageProps,
 }: {
-  Component: any
-  pageProps: any
+  Component: any;
+  pageProps: any;
 }) {
   return (
-    <div lang="en">
-      <Component {...pageProps}/>
-    </div>
-  )
+    <CacheProvider value={clientSideEmotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
+  );
 }
