238d25124dc150de50fab66cd6e47737acc5a725
SpiderPigzz <Ricky.winarko@gmail.com>
2023-08-02 03:06:00 +0800

    Modified margins

---
+++ components/ToggleButton.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { Typography } from "@mui/material";
+
+export default function ToggleGroup({ MultiOption, option, setOption }) {
+  const handleChange = (event, choices) => {
+    setOption(choices);
+  };
+
+  return (
+    <>
+      <ToggleButtonGroup
+        color="primary"
+        value={option}
+        exclusive
+        onChange={handleChange}
+        aria-label="Platform"
+        sx={{ paddingBottom: 2 }}
+      >
+        {MultiOption.map((choices, idx) => (
+          <ToggleButton value={choices} key={idx}>
+            <Typography sx={{ textTransform: "uppercase" }}>
+              {choices}
+            </Typography>
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </>
+  );
+}
