ebc674a36fb59af523d7eae24944465c00916dba
SpiderPigzz <Ricky.winarko@gmail.com>
2023-08-05 01:38:53 +0800

    Update App.jsx

--- pages/PhotoDump/App.jsx
+++ pages/PhotoDump/App.jsx
@@ -62,6 +62,7 @@ export default function App() {
             flexDirection: "column",
             display: "flex",
             justifyContent: "center",
+            width: "100%",
           }}
         >
           <img
