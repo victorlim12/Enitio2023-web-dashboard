b77d503c89f72497d4b6cad8994907968cd829d6
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2023-08-05 17:59:19 +0800

    test update css for scrollbar

--- pages/ScoreBoard/SubBlock/Leaderboard.jsx
+++ pages/ScoreBoard/SubBlock/Leaderboard.jsx
@@ -32,7 +32,7 @@ export default function LeaderBoard() {
         setOption={setOption}
       />
       <div
-        className="visible-scrollbar"
+        className="invisible-scrollbar"
         style={{ overflowY: "scroll", minHeight: "27rem", maxHeight: "27rem" }}
       >
         {option === "Clan" ? (
